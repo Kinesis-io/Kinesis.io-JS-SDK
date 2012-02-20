a85f61347de822a8c473dc15418d22117fd69bb3
Achal Aggarwal <aggarwal.achal@gmail.com>
2012-02-20 15:38:52 +0530

    changes to node app

--- app.js
+++ app.js
@@ -11,14 +11,11 @@ setHeaders = function (req,res,next) {
 Server.configure(function(){
   Server.set('views', __dirname + '/app/views');
   Server.set('view engine', 'ejs');
+
   Server.use(express.static(__dirname + '/public'));
+  Server.use(express.directory(__dirname + '/public'));
   Server.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
   Server.use(setHeaders);
-});
-
-// application paths
-Server.get('/', function(req, res){
-  res.send('Kiosk Server Running');
 });
 
 // start server
